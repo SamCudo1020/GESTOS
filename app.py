2b5eb236d8599e34bfdbe015be146fdf04c3eb42
SamCudo1020 <chamuel567@gmail.com>
2026-03-25 21:47:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.write("Versión de Python:", platform.python_version())
 model = load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-st.title("Reconocimiento de Imágenes YAAAAY:D")
+st.title("Super reconocedor de imagenes 3000")
 #st.write("Versión de Python:", platform.python_version())
 image = Image.open('pensative.jpeg')
 st.image(image, width=350)
